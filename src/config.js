d99c8fb1c13e847834abc33d401e6a650ebe8623
ihanbingi <138949044+ihanbingi@users.noreply.github.com>
2026-07-09 17:11:53 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -144,8 +144,8 @@ const config = {
 		{ go: "👍 前往", img: "/img/sunshine.jpg", title: "Project 4", subtitle: "4,000 miles of wonder", text: "If you see this line, I've managed to get your attention.", url: "https://leleo.top", show: false },
 		{ go: "🗃 前往", img: "/img/sunshine.jpg", title: "Project 5", subtitle: "5,000 miles of wonder", text: "If you see this line, I've managed to get your attention.", url: "https://leleo.top", show: false },
 		{ go: "🎨 前往", img: "/img/sunshine.jpg", title: "Project 6", subtitle: "6,000 miles of wonder", text: "If you see this line, I've managed to get your attention.", url: "https://leleo.top", show: false },
-		{ go: "💍 前往", img: "做的一些东西", title: "Project 7", subtitle: "7,000 miles of wonder", text: "If you see this line, I've managed to get your attention.", url: "https://ihanbingi.github.io/download", show: false },
-		{ go: "🔍 前往", img: "/img/sunshine.jpg", title: "Project 8", subtitle: "8,000 miles of wonder", text: "If you see this line, I've managed to get your attention.", url: "https://ihanbingi.github.io/more", show: false },
+		{ go: "💍 前往", img: "/img/sunshine.jpg", title: "原创作品", subtitle: "7,000 miles of wonder", text: "If you see this line, I've managed to get your attention.", url: "https://ihanbingi.github.io/download", show: false },
+		{ go: "🔍 前往", img: "/img/sunshine.jpg", title: "更多", subtitle: "8,000 miles of wonder", text: "If you see this line, I've managed to get your attention.", url: "https://ihanbingi.github.io/more", show: false },
 	],
 
 	statement: ["备案号：XXICP备123456789号", "Copyright © 2025 Leleo"],
