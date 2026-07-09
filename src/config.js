9613d542ed542ac68096acb2456cc28dd985b636
ihanbingi <138949044+ihanbingi@users.noreply.github.com>
2026-07-09 23:30:02 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -71,7 +71,6 @@ const config = {
 
 	//音乐播放配置，采用MetingJS Api(https://github.com/metowolf/MetingJS)
 	musicPlayer: {
-		autoplay: 'true',
 		server: 'netease',  //服务提供商 --网易云音乐
 		type: 'playlist',   //歌单类型
 		id: '13697316223'  //歌单id ---> music.163.com/#/playlist?id=2028178887
