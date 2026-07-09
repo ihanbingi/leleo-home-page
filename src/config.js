0405b1c92a5adefbfad503aaf0de1788f4b2b76d
ihanbingi <138949044+ihanbingi@users.noreply.github.com>
2026-07-09 23:26:52 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -74,7 +74,7 @@ const config = {
 		autoplay: 'true',
 		server: 'netease',  //服务提供商 --网易云音乐
 		type: 'playlist',   //歌单类型
-		id: '7079411923'  //歌单id ---> music.163.com/#/playlist?id=2028178887
+		id: '13697316223'  //歌单id ---> music.163.com/#/playlist?id=2028178887
 	},
 
 	//壁纸数据 -----可以将壁纸文件上传到图床获取网络直链。若想调用api，请前往脚本自行修改逻辑
